cd3ce2a5c703f6c41435d75e734f000de61e7778
lucasbagge <36770549+lucasbagge@users.noreply.github.com>
2023-02-27 16:03:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,5 @@ setup(name="git-for-pip-example",
       version="1.0.0",
       description="An example package for showing how to pip install from a git repo",
       license='MIT',
-      author="Matias Codesal",
-      authoer_email="matias.codesal@gmail.com",
-      url='https://github.com/matiascodesal/git-for-pip-example',
-      packages=['distutils', 'distutils.command', 'pandas', 'numy']
+      packages=['pandas', 'numy']
 )
