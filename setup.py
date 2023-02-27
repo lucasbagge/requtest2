8e7a6f1bb468e1a164a1a87c97ddebf42c4b15fe
lucasbagge <36770549+lucasbagge@users.noreply.github.com>
2023-02-27 16:11:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from distutils.core import setup
 
 setup(name="git-for-pip-example",
-      version="1.0.0",
-      description="An example package for showing how to pip install from a git repo",
       license='MIT',
       install_requires=['pandas', 'numy']
 )
